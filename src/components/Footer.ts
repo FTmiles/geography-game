564180a70468a56b3b4156d3d324d7c1f8daeb2e
Modestas Katinas <ftmiles@proton.me>
2024-12-28 00:40:56 +0200

    added github actions CI/CD
    * added github actions
    
    * adding to ssh to known hosts
    
    * fixed upload to EC2 instructions
    
    * fixing uploding files
    
    * readme
    
    * 2025 footer update
    
    ---------
    
    Co-authored-by: Swee X <phat@bro.com>

--- src/components/Footer.ts
+++ src/components/Footer.ts
@@ -2,7 +2,7 @@
 export  default function Footer(){
     const footerDom = document.querySelector('footer');
     const copyright = document.createElement('p');
-    copyright.innerHTML = "Copyright &copy; 2024";
+    copyright.innerHTML = "Copyright &copy; 2025";
     copyright.className = "text-center";
     footerDom?.append(copyright);
 
